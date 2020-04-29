119fa6cf9496c3295ceaf917775cc38ef376cf62
shatakshi <rastogi.prakhar@live.com>
2020-04-29 16:38:24 +0530

    ribbon

--- cart-microservice/src/main/java/com/infogain/ecommerce/cart/CartController.java
+++ cart-microservice/src/main/java/com/infogain/ecommerce/cart/CartController.java
@@ -3,6 +3,7 @@ package com.infogain.ecommerce.cart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -13,6 +14,7 @@ import java.io.IOException;
 
 @CrossOrigin
 @RestController
+@RibbonClient(name="cart")
 public class CartController extends ResponseEntityExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(CartController.class);
