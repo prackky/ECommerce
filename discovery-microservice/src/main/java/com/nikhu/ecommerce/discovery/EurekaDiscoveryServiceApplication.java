5e88c2bbae9ebb5f19ea1a0093bf429c19bfd1a4
shatakshi <rastogi.prakhar@live.com>
2020-04-29 10:31:33 +0530

    first commit

---
+++ discovery-microservice/src/main/java/com/nikhu/ecommerce/discovery/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,19 @@
+package com.nikhu.ecommerce.discovery;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by VRavuri on 01-11-2016.
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+    }
+}
---
+++ product-catalog-microservice/src/main/java/com/nikhu/ecommerce/product/ProductCatalogRepository.java
@@ -0,0 +1,13 @@
+package com.nikhu.ecommerce.product;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductCatalogRepository extends MongoRepository<Product, Long> {
+
+	void deleteById(String prodId);
+
+	Optional<Product> findById(String prodId);
+
+}
